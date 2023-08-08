21d12dbb5bac6396948781be4676b467c7760c81
Stephanie Frantz <stephlage@gmail.com>
2023-08-07 21:05:26 -0400

    init

--- install.sh
+++ install.sh
@@ -1,14 +1,23 @@
 sudo apt update
-sudo apt install git vim zsh install software-properties-common apt-transport-https wget -y
+sudo apt -qq install git vim zsh apt-transport-https curl software-properties-common
 
+## vs code
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt install code
 code --version
 
 
+## zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/master/tools/install.sh -o -)"
 sudo chsh -s $(which zsh) $(whoami)
 
+## brave browser
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt install brave-browser
+
+
 echo "You should restart the machine so that zsh works nice."
+
 
