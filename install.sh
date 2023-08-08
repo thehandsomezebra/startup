c2df39712856ebc68588d6f1b6b9ae724dbdf02e
Stephanie Frantz <stephlage@gmail.com>
2023-08-07 21:09:55 -0400

    init

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt install git vim zsh apt-transport-https curl software-properties-common
 ## vs code
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update
 sudo apt install code -y -qq
 code --version
 
@@ -15,6 +16,7 @@ sudo chsh -s $(which zsh) $(whoami)
 ## brave browser
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
 sudo apt install brave-browser -y -qq
 
 
